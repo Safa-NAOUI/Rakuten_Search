9ec03536fdd317c2c9024928d20f1bd4c70cb5e9
Safa-NAOUI <safa.naoui96@gmail.com>
2025-01-15 18:12:21 +0100

    clean code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.example.akuten"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.example.rakuten"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
