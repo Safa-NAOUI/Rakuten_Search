6967498dccbc337d7025a35d4c2ed847f1de7e38
Safa-NAOUI <safa.naoui96@gmail.com>
2025-01-17 02:24:54 +0100

    clean code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,44 +88,56 @@ android {
 }
 
 dependencies {
-    // Core AndroidX and Kotlin libraries
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(libs.kotlin.stdlib)
-
-    // Jetpack Compose
-    implementation(platform(libs.androidx.compose.bom)) // BOM for managing Compose versions
-    implementation(libs.compose.ui)
-    implementation(libs.compose.material)
-    implementation(libs.compose.ui.tooling.preview)
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
 
-    // Hilt for Dependency Injection
+    /** jetPack compoise **/
+    implementation(libs.compose.ui)
+    implementation(libs.compose.material)
+    implementation(libs.compose.ui.tooling.preview)
+
+    /** Hilt DI **/
     implementation(libs.hilt.android)
-    kapt(libs.hilt.android.compiler)
+    implementation(libs.hilt.android.compiler)
 
-    // Retrofit & Gson for networking
+    /** Retrofit & Gson **/
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.okhttp.logging.interceptor)
     implementation(libs.navigation.compose)
 
-    // Firebase
-    implementation(platform(libs.firebase.bom))
+    /** Firebase **/
+    implementation(libs.firebase.bom)
     implementation(libs.firebase.analytics)
 
-    // Test dependencies
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-
-    // Debug dependencies
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    /** Additional dependencies **/
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
 }
