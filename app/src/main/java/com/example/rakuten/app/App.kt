19d37c8499854a44087dd4570f0548d6590955f1
Safa-NAOUI <safa.naoui96@gmail.com>
2025-01-21 11:56:43 +0100

    Create an application class

---
+++ app/src/main/java/com/example/rakuten/app/App.kt
@@ -0,0 +1,11 @@
+package com.example.rakuten.app
+
+/**
+ * Created by Safa NAOUI on 19,January,2025
+ */
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
