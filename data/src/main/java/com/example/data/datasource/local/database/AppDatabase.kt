940ee824efaecfe4b851fb44886c2b5d598074aa
Safa-NAOUI <safa.naoui96@gmail.com>
2025-01-21 11:31:21 +0100

    add database ( module data )

---
+++ data/src/main/java/com/example/data/datasource/local/database/AppDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.data.datasource.local.database
+
+/**
+ * Created by Safa NAOUI on 20,January,2025
+ */
+
+import com.example.data.datasource.local.dao.ProductDao
+import com.example.data.datasource.local.entity.ProductEntity
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.data.datasource.local.dao.ProductDetailDao
+import com.example.data.datasource.local.entity.ProductDetailEntity
+import com.example.data.utils.Converters
+
+/**
+ * The main database class for the application, using Room as the database framework.
+ * Defines the database schema, entities, and provides DAO access.
+ *
+ * @property productDao Provides access to product-related database operations.
+ * @property productDetailDao Provides access to product detail-related database operations.
+ */
+
+@Database(
+    entities = [ProductEntity::class, ProductDetailEntity::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun productDao(): ProductDao
+    abstract fun productDetailDao(): ProductDetailDao
+}
