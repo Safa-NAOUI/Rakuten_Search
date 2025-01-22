f3c36195966b320d3de7e301f3d64b0aa0a81533
Safa-NAOUI <safa.naoui96@gmail.com>
2025-01-22 21:47:17 +0100

    add getProductDetails to LocalProductDataSource

--- data/src/main/java/com/example/data/datasource/local/LocalProductDataSource.kt
+++ data/src/main/java/com/example/data/datasource/local/LocalProductDataSource.kt
@@ -1,10 +1,13 @@
 package com.example.data.datasource.local
 
+import android.util.Log
 import com.example.data.datasource.LocalDataSource
 import com.example.data.datasource.local.dao.ProductDao
 import com.example.data.mapper.toDomain
 import com.example.domain.model.Product
+import com.example.domain.model.ProductDetail
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
 
 import javax.inject.Inject
 
@@ -20,4 +23,14 @@ class LocalProductDataSource @Inject constructor(
     override suspend fun searchProducts(keyword: String): List<Product> {
         return productDao.getAllProducts(keyword).first().map { it.toDomain() }
     }
+
+    /**
+     * Retrieves product details from the local database.
+     * Returns `null` if the product is not found.
+     */
+    override suspend fun getProductDetails(id: String): ProductDetail {
+        Log.d("getProductDetailsaaaaaaaaaaaaa", "Product State: "+productDao.getProductDetail(id).toString())
+       return productDao.getProductDetail(id).firstOrNull()!!.toDomain() ///?: api.getProductDetails(id).toDomain()
+    }
 }
+
