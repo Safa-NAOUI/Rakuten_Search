a337401f913a43bbc737b62c7155762eef86deff
Safa-NAOUI <safa.naoui96@gmail.com>
2025-01-22 14:59:38 +0100

    add class navigation

---
+++ app/src/main/java/com/example/rakuten/app/navigation/RakutenNav.kt
@@ -0,0 +1,17 @@
+package com.example.rakuten.app.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.rakuten.app.ui.screens.SearchScreen
+
+@Composable
+fun RakutenNav() {
+    val navController = rememberNavController()
+
+    NavHost(navController, startDestination = "search") {
+        composable("search") { SearchScreen(navController) }
+
+    }
+}
