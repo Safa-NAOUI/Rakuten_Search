6d2abae951386dd764ee6767d4269be9567e1b37
Safa-NAOUI <safa.naoui96@gmail.com>
2025-01-23 12:39:43 +0100

    update Cucumber test

--- app/src/androidTest/java/com/example/rakuten/bdd/CucumberTestRunner.kt
+++ app/src/androidTest/java/com/example/rakuten/bdd/CucumberTestRunner.kt
@@ -1,5 +1,6 @@
 package com.example.rakuten.bdd
 
+import dagger.hilt.android.testing.HiltAndroidTest
 import io.cucumber.junit.Cucumber
 import io.cucumber.junit.CucumberOptions
 import org.junit.runner.RunWith
@@ -8,7 +9,7 @@ import org.junit.runner.RunWith
 /**
  * Created by Safa NAOUI on 17,January,2025
  */
-
+@HiltAndroidTest
 @RunWith(Cucumber::class)
 @CucumberOptions(
     features = ["classpath:src/androidTest/resources/features"],
--- app/src/androidTest/java/com/example/rakuten/bdd/steps/SearchSteps.kt
+++ app/src/androidTest/java/com/example/rakuten/bdd/steps/SearchSteps.kt
@@ -1,26 +1,50 @@
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.navigation.compose.rememberNavController
+import com.example.rakuten.app.ui.screens.SearchScreen
+import com.example.rakuten.app.viewmodel.ProductViewModel
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.When
 import io.cucumber.java.en.Then
+import org.junit.Rule
+import org.mockito.Mockito.mock
+
 
 class SearchSteps {
 
+    // Create a rule for Compose tests
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+
+   private val viewModel: ProductViewModel = mock()
+
     @Given("the user is on the search screen")
     fun userIsOnSearchScreen() {
-        // Code to ensure the user is on the search screen
+        composeTestRule.setContent {
+            SearchScreen(navController = rememberNavController(), viewModel = viewModel)
+        }
     }
 
     @When("the user enters {string} in the search bar")
     fun userEntersKeyword(keyword: String) {
-        // Code to type the keyword in the search bar
+        composeTestRule.onNodeWithTag("search_bar")
+            .performTextInput(keyword)
     }
 
     @When("the user taps the search button")
     fun userTapsSearchButton() {
-        // Code to tap the search button
+        composeTestRule.onNodeWithTag("search_button")
+            .performClick()
     }
 
     @Then("the user sees a list of products")
     fun userSeesListOfProducts() {
-        // Code to verify the list of products is displayed
+        composeTestRule.onNodeWithTag("product_list")
+            .assertIsDisplayed()
     }
 }
+
