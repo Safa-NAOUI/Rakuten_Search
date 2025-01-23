cad766afbfca5d70699fdae57c1b6053fd26ce71
Safa-NAOUI <safa.naoui96@gmail.com>
2025-01-23 15:01:49 +0100

    Update MainActivity

--- app/src/main/java/com/example/rakuten/app/ui/screens/MainActivity.kt
+++ app/src/main/java/com/example/rakuten/app/ui/screens/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MaterialTheme {
+            MyApplicationTheme {
                 RakutenNav()
             }
         }
